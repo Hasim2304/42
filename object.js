26f104a350b91b6a5d15068e4107add67b663c0a
Hasim2304 <hk828072@gmail.com>
2024-09-23 17:40:50 +0530

    first commit

---
+++ object.js
@@ -0,0 +1,23 @@
+// var hasim={}
+// hasim.name="vijay",
+// hasim.age= 30,
+// hasim.city="madurai",
+// hasim.Education={
+//         college:"wakf Board college",
+//         yearpassing:2021-2024,
+//         Degree:"B.com",
+//     }
+    
+
+
+// console.log(hasim.Education);
+
+// function hasim(){
+var meter=document.getElementById('hasim').value
+
+var kilometer=meter*1000
+document.getElementById('para').innerHTML=kilometer
+// }
+
+
+
